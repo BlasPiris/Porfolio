de87b0c15a677e4a294b72d4f5a71fd20c19b0ca
Blas Piris <50118504+BlasPiris@users.noreply.github.com>
2024-05-09 20:18:36 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -5,9 +5,9 @@ fetch(apiUrl)
     //const reposList = document.getElementById('repos-list'); // Cambia esto al ID de tu elemento HTML donde deseas mostrar la lista
     data.forEach(repo => {
         //console.log(repo);
-        console.log(repo.html_url);
-        console.log(repo.name);
-        console.log(repo.language);
+        // console.log(repo.html_url);
+         //console.log(repo.name);
+         //console.log(repo.language);
       //const repoLink = document.createElement('a');
     //   repoLink.href = repo.html_url;
     //   repoLink.textContent = repo.name;
